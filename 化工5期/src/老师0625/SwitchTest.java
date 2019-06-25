8194a85d04d01c037d61de06a9de87629e2d97b9
'nicholasleilei' <349417084@qq.com>
2019-06-25 17:08:44 +0800

    123

---
+++ 化工5期/src/老师0625/SwitchTest.java
@@ -0,0 +1,22 @@
+package 老师0625;
+
+public class SwitchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		String str ="aaa";
+         switch(str) {//
+         case "aaa":
+        	 System.out.println("是aaa");
+        	 break;
+         case "bbb":
+        	 System.out.println("是bbb");
+        	 break;
+        	default:
+        		System.out.println("以上都不是");
+        	 
+         }
+	}
+
+}
---
+++ 化工5期/src/老师0625/SwitchTest01.java
@@ -0,0 +1,42 @@
+package 老师0625;
+
+import java.util.Scanner;
+
+public class SwitchTest01 {
+
+	public static void main(String[] args) {
+		// 键盘录入一个数，根据这个数输出对应的星期
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入一个数：");
+		int n = sc.nextInt();
+		switch (n) {
+		case 1:
+			System.out.println("星期一");
+			break;
+		case 2:
+			System.out.println("星期二");
+			break;
+		case 3:
+			System.out.println("星期三");
+			break;
+		case 4:
+			System.out.println("星期四");
+			break;
+		case 5:
+			System.out.println("星期五");
+			break;
+		case 6:
+			System.out.println("星期六");
+			break;
+		case 7:
+			System.out.println("星期日");
+			break;
+
+		default:
+			System.out.println("录入错误");
+			break;
+		}
+	}
+
+}
