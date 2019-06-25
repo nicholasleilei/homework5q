b9081d69bcf9fb656e78a4908d691c6cad9d1d43
'nicholasleilei' <349417084@qq.com>
2019-06-25 17:36:10 +0800

    999

---
+++ 化工5期/src/老师0625/WhileTest.java
@@ -0,0 +1,18 @@
+package ¿œ ¶0625;
+
+public class WhileTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//           if£®£©{}
+		int i=1;
+		while(i<=10) {
+			System.out.println("Hello");
+			i++;
+		}
+		
+		
+
+	}
+
+}
