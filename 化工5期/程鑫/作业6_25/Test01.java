7153384ebc0fe3cd6124f12835140a90ca56048c
'nicholasleilei' <349417084@qq.com>
2019-06-25 16:22:16 +0800

    6月25日 程鑫完成作业

---
+++ 化工5期/程鑫/作业6_25/Test01.java
@@ -0,0 +1,12 @@
+package ื๗าต6_25;
+
+import java.util.Scanner;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner sc = new Scanner(System.in);
+	}
+
+}
