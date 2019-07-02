1068933eec04050fdcd5c898957b835fb356e2d9
The Shy <The Shy@DESKTOP-SQV4IUC>
2019-07-02 15:53:23 +0800

    00

---
+++ 化工5期/src/程鑫/TEST.java
@@ -0,0 +1,28 @@
+package 程鑫;
+
+public class TEST {
+public static void main(String[] args) {
+	int a=0;
+	int sum1=0;
+	while(a<=100) {
+	sum1=sum1+a;
+	a+=2;
+	}
+	System.out.println("奇数和:"+sum1);
+	int b=1;
+	int sum2=0;
+	while(b<=100) {
+	sum2=sum2+b;
+	b+=2;
+	}
+	System.out.println("偶数和:"+sum2);
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
